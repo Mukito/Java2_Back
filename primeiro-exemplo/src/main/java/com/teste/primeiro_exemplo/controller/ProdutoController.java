bbb17b9c6fb078dc551189afe9f02ea6a3a94397
Fabiano Santos Ramos <mukito100@gmail.com>
2025-08-09 13:52:58 -0300

    Update ProdutoController.java

--- primeiro-exemplo/src/main/java/com/teste/primeiro_exemplo/controller/ProdutoController.java
+++ primeiro-exemplo/src/main/java/com/teste/primeiro_exemplo/controller/ProdutoController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teste.primeiro_exemplo.model.Produto;
 import com.teste.primeiro_exemplo.services.ProdutoService;
-import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -50,3 +51,4 @@ public class ProdutoController {
     }
 
 }
+
